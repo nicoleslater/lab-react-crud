2a4027595fcef6e93f16a1a88e0ce19ab41893f9
Nicole Slater <nicoleslater@pursuit.org>
2023-08-14 21:57:07 -0400

    Update

--- src/components/movies/MoviesIndex.jsx
+++ src/components/movies/MoviesIndex.jsx
@@ -1,4 +1,12 @@
 export default function MoviesIndex() {
+  const [loadingError, setLoadingError] = useState(true);
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("URL/api/movies")
+    .then((res) => res.json())
+    .then((json) => setMovies(json));
+  }, []);
+  
   return <p>Movie List</p>;
 }
