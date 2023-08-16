2fa73f9d6639783cf217ce1076f92a96dbf1de13
Nicole Slater <nicoleslater@pursuit.org>
2023-08-16 12:23:36 -0400

    Movie Index

--- src/components/movies/MoviesIndex.jsx
+++ src/components/movies/MoviesIndex.jsx
@@ -1,12 +1,52 @@
+import { getAllMovies } from "../../api/fetch";
+
 export default function MoviesIndex() {
   const [loadingError, setLoadingError] = useState(true);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch("URL/api/movies")
-    .then((res) => res.json())
-    .then((json) => setMovies(json));
-  }, []);
+    // we need to get data 
+    getAllMovies()
+      .then((movieJson) => {
+        setShows(movieJsonJson)
+        setLoadingError(false)
+      })
+      .catch((err)=> {
+        setLoadingError(true);
+        console.error(err)
+      })
+    // and save it to our shows  state
+    // dependency array tells us what to follow to fire our useEffect
+  },[])
   
-  return <p>Movie List</p>;
+  return (
+    <div>
+  { loadingError ? (
+    <ErrorMessage />
+  ) : (
+    <section className="movies-index-wrapper">
+      <h2>All Movies</h2>
+      <button>
+        <Link to="/shows/new">Add a new movie</Link>
+      </button>
+      <br />
+      <label htmlFor="searchTitle">
+        Search Movies:
+        <input
+          type="text"
+          // value={searchTitle}
+          id="searchTitle"
+          // onChange={handleTextChange}
+        />
+      </label>
+      <section className="movies-index">
+        { movies.map((movie) => {
+          return <MovieListing movie = {movie} key = {movie.id}/>
+        })}
+      </section>
+    </section>
+  )}
+</div>
+);
 }
+
