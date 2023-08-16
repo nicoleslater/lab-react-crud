6b5b0e22612029c1e207e4a05bb108bef17c8a9e
Nicole Slater <nicoleslater@pursuit.org>
2023-08-16 14:32:47 -0400

    Import Link and Error Message

--- src/components/movies/MoviesIndex.jsx
+++ src/components/movies/MoviesIndex.jsx
@@ -1,5 +1,7 @@
 import { getAllMovies } from "../../api/fetch";
 import "../shows/ShowsIndex.css"
+import { Link } from "react-router-dom";
+import ErrorMessage from "../errors/ErrorMessage";
 
 export default function MoviesIndex() {
   const [loadingError, setLoadingError] = useState(true);
