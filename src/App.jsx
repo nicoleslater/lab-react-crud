fa853287459eec482ffc5883290b945f5cefda11
Nicole Slater <nicoleslater@pursuit.org>
2023-08-16 21:52:48 -0400

    CSS

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
           <Route path="/shows/new" element={<ShowsNewForm />} />
           <Route path="/shows/:id" element={<Show />} />
           <Route path="/shows/:id/edit" element={<ShowsEditForm />} />
-        <Route path="/movies" element={<MoviesIndex />} />
+          <Route path="/movies" element={<MoviesIndex />} />
           <Route path="/movies/new" element={<MoviesNewForm />} />
           <Route path="/movies/:id" element={<Movie />} />
           <Route path="/movies/:id/edit" element={<MoviesEditForm />} />
